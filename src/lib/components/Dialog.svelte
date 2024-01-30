65c44f965b8632625058b5bc745b9682d48637df
Adam Shand <adam@shand.net>
2024-01-31 11:02:35 +1300

    example dialog component

---
+++ src/lib/components/Dialog.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	let dialog: HTMLDialogElement
+
+	export function toggle() {
+		dialog.open ? close() : open()
+	}
+	export function open() {
+		dialog.showModal()
+	}
+	export function close() {
+		dialog.close()
+	}
+</script>
+
+<dialog bind:this={dialog}>
+	<form method="dialog">
+		<slot />
+		<button>OK</button>
+	</form>
+</dialog>
+
+<style>
+	::backdrop {
+		background: #0004;
+		backdrop-filter: blur(2px);
+		opacity: 0;
+		animation: 0.3s fade-in forwards;
+	}
+
+	dialog[open] {
+		padding: var(--size-7);
+		color: var(--gray-2);
+		border-radius: var(--radius-2);
+		animation: var(--animation-slide-in-up) forwards;
+	}
+
+	dialog form {
+		display: flex;
+		flex-direction: column;
+		gap: var(--size-4);
+		justify-content: flex-end;
+	}
+</style>
---
+++ src/routes/_play/dialog/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Dialog from '$lib/components/Dialog.svelte'
+
+	let dialog: Dialog
+</script>
+
+<h1>Dialog example</h1>
+
+<button on:click={dialog.toggle}>Toggle</button>
+
+<Dialog bind:this={dialog}>
+	<p>This is a dialog!</p>
+</Dialog>
+
+<style>
+	:global(body) {
+		background: none;
+		padding: 4rem;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 2rem;
+	}
+
+	h1 {
+		color: var(--blue-4);
+	}
+
+	:global(button) {
+		color: var(--blue-3);
+	}
+</style>

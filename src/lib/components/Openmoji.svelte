53ac794e569bf3ba1538ccd22be5aecb09897dc0
Adam Shand <adam@shand.net>
2024-07-17 16:35:54 +1200

    add title prop

--- src/lib/components/Openmoji.svelte
+++ src/lib/components/Openmoji.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	export let id: string
 	export let size: 's' | 'm' | 'l' = 'm'
+	export let title: string = ''
+
 	const src =
 		'https://cdn.jsdelivr.net/gh/hfg-gmuend/openmoji/color/svg/' + id.toUpperCase() + '.svg'
 </script>
 
-<img alt="Openmoji icon" class={size} {src} />
+<img alt="Openmoji icon" class={size} {src} {title} />
 
 <style>
 	.s {
